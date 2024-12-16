c710794f76c2e07a6e0a3ea482b814f45f13fbd5
freemuras <freemuras@gmail.com>
2024-12-16 16:44:03 +0900

    FIX
    JSON 경로 수정. 실행명령어
    pnpm paws-time:dev
    또는
    pnpm --filter paws-time dev

---
+++ packages/design-system/lib/thema/colors.ts
@@ -0,0 +1,13 @@
+export const referenceToken = {
+  base: {
+    white: "#fffff",
+    gray: "#C4C4C4",
+    black: "#000000",
+  },
+
+  primary: {},
+};
+
+export const systemToken = {};
+
+export const componentToken = {};
