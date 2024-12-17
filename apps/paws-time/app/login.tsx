affab94dc59f15ceba5175ddd62ca6a5ad093a4a
hiide <ssm5170@gmail.com>
2024-12-17 17:35:32 +0900

    feat:로그인페이지

---
+++ apps/paws-time/app/login.tsx
@@ -0,0 +1,57 @@
+// np -> snippets
+
+import { NextPage } from "next";
+import { useState } from "react";
+
+interface Props {
+  email: string;
+  password: string;
+}
+
+const Login: NextPage<Props> = () => {
+  const [loginValue, setLoginValue] = useState<Props>({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginValue((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // 새로고침 방지
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={loginValue.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={loginValue.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
