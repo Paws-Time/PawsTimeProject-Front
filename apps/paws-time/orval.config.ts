71f98197c6917caf4c4f2e2bcb95fb4ee10be43a
freemuras <freemuras@gmail.com>
2024-12-21 19:40:35 +0900

    orval 설치

---
+++ apps/paws-time/orval.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  pawsTime: {
+    input: "app/lib/codegen/schema.json",
+    output: {
+      client: "react-query",
+      target: "app/lib/codegen/hooks", // ? 만들어질 hook들의 경로
+      schemas: "app/lib/codegen/dtos", // ? 만들어질 hook에 사용될 type들의 경로
+      mode: "tags-split",
+      prettier: true,
+
+      override: {
+        query: {
+          useQuery: true,
+          useInfinite: true,
+          useMutation: true,
+        },
+        mutator: {
+          path: "app/lib/axios-client/customClient.ts",
+          name: "customInstance",
+        },
+      },
+    },
+  },
+});
